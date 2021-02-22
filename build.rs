2ec62ab91032738f5607a019d52931ce63c8711c
Maarten de Klerk <maarten@deklerk.dev>
2021-02-22 22:16:35 +0100

    fix: update whitelist, remove comments and use mbedtls

--- build.rs
+++ build.rs
@@ -3,31 +3,31 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+  println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
+  println!("cargo:rustc-link-lib=static=mbedtls");
+  println!("cargo:rustc-link-lib=static=mbedcrypto");
+  println!("cargo:rustc-link-lib=static=mbedx509");
+
   let mut dst = Config::new("libcoap")
-    .define("DTLS_BACKEND", "tinydtls")
+    .define("DTLS_BACKEND", "mbedtls")
     .define("ENABLE_TESTS", "OFF")
     .define("ENABLE_EXAMPLES", "OFF")
     .define("ENABLE_DOCS", "OFF")
     .build();
 
-  dst.push("build");
-
-  println!("cargo:rustc-link-search=native={}", dst.display());
-  println!("cargo:rustc-link-lib=static=coap-2");
-
   dst.push("lib");
 
   println!("cargo:rustc-link-search=native={}", dst.display());
-  println!("cargo:rustc-link-lib=static=tinydtls");
+  println!("cargo:rustc-link-lib=static=coap-2");
 
   println!("cargo:rerun-if-changed=wrapper.h");
 
   let bindings = bindgen::Builder::default()
     .header("wrapper.h")
-    .generate_comments(true)
-    .use_core()
-    .whitelist_type("coap_.*")
-    .whitelist_function("coap_.*")
+    .generate_comments(false) // true breaks the binding
+    .whitelist_var("^(?i)coap_.*")
+    .whitelist_type("^(?i)coap_.*")
+    .whitelist_function("^(?i)coap_.*")
     .generate()
     .expect("Unable to generate bindings");
 
