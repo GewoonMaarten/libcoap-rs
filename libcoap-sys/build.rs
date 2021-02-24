e6194ae74a54946067d8ddf44139eff459640f50
Maarten de Klerk <maarten@deklerk.dev>
2021-02-24 20:26:26 +0100

    refactor: use format macro instead of pathbuf

--- libcoap-sys/build.rs
+++ libcoap-sys/build.rs
@@ -1,6 +1,4 @@
-use cmake::Config;
 use std::env;
-use std::path::PathBuf;
 
 fn main() {
   println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
@@ -8,7 +6,7 @@ fn main() {
   println!("cargo:rustc-link-lib=static=mbedcrypto");
   println!("cargo:rustc-link-lib=static=mbedx509");
 
-  let mut dst = Config::new("libcoap")
+  let mut dst = cmake::Config::new("libcoap")
     .define("DTLS_BACKEND", "mbedtls")
     .define("ENABLE_TESTS", "OFF")
     .define("ENABLE_EXAMPLES", "OFF")
@@ -20,22 +18,21 @@ fn main() {
   println!("cargo:rustc-link-search=native={}", dst.display());
   println!("cargo:rustc-link-lib=static=coap-2");
 
-  println!("cargo:rerun-if-changed=wrapper.h");
+  println!("cargo:rerun-if-changed=libcoap");
 
   let out_dir = env::var("OUT_DIR").unwrap();
 
   let bindings = bindgen::Builder::default()
     .header(format!("{}/build/include/coap2/coap.h", out_dir))
     .clang_arg(format!("-I{}/include/coap2", out_dir))
-    .generate_comments(false) // true breaks the binding
+    .generate_comments(false) // true breaks the binding. see: https://github.com/rust-lang/rust-bindgen/issues/426
     .whitelist_var("^(?i)(lib)?coap_.*")
     .whitelist_type("^(?i)(lib)?coap_.*")
     .whitelist_function("^(?i)(lib)?coap_.*")
     .generate()
     .expect("Unable to generate bindings");
 
-  let out_path = PathBuf::from(out_dir);
   bindings
-    .write_to_file(out_path.join("bindings.rs"))
+    .write_to_file(format!("{}/bindings.rs", out_dir))
     .expect("Couldn't write bindings!");
 }
